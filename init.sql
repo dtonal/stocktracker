33f7aa275f44730656640e98a48017ee16d295fc
dtonal <dtonal@posteo.de>
2025-07-22 15:52:37 +0200

    preparing production ready deployment

---
+++ init.sql
@@ -0,0 +1,3 @@
+-- Aktiviert eine Erweiterung, die wir für UUIDs in der Datenbank benötigen könnten.
+-- Es schadet nicht, sie standardmäßig zu aktivieren.
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp"; 
